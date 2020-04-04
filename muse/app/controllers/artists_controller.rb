58de3e16b60816a66a5d9b04e9567f520f73fa23
Renad-Ahmad <59873078+Renad-Ahmad@users.noreply.github.com>
2020-04-04 17:19:26 +0300

    Update artists_controller.rb

--- muse/app/controllers/artists_controller.rb
+++ muse/app/controllers/artists_controller.rb
@@ -1,57 +1,56 @@
 class ArtistsController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create, :index, :destroy, :update, :edit]
-    before_action :find_artist, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new , :edit , :destroy]
+  before_action :find_artist, except: [:index, :new, :create]
+  def index
+    @artists = Artist.all
+  end
 
-        def index
-            @artists = current_user.artists.all
-        end
+  def show
+    @artist = Artist.find(params[:id])
+    @songs = @artist.songs
+    if @artist.user !=  current_user
+      flash[:notice] = 'Not allowed!'
+      redirect_to artists_path
+    end
+   
+  end
 
-        def show
-            if @artist.user != current_user
-                flash[:notice] = "Not Allowed!"
-                redirect_to artists_path
-            end
+  def new
+    @artist = Artist.new
+  end
 
-            @songs = @artist.songs
-        end
+  def create
+    @artist=Artist.new(person_params)
+    @artist.user = current_user
+    if @artist.save
+        
+      redirect_to @artist
+     else 
+      render 'new'
+     end
+   
+  end
 
-        def new
-            @artist = Artist.new
-        end
+  def edit
+    @artist = Artist.find(params[:id])
+  end
 
-        def create
-            @artist = Artist.new(person_params)
+  def update
+    artist = Artist.find(params[:id])
+    artist.update(person_params)
+      
+    redirect_to artist
+  end
 
-            @artist.user = current_user
+  def destroy
+    Artist.find(params[:id]).destroy
+  
+    redirect_to artists_path
+  end
 
-            if @artist.save
-                redirect_to artists_path
-            else
-                render :new
-            end
-        end
-
-        def edit
-            @artist
-        end
-
-        def update
-            @artist.update(person_params)
-
-            redirect_to @artist
-        end
-
-        def destroy
-            @artist.destroy
-
-            redirect_to artists_path
-        end
-
-    private
-        def person_params
-            params.require(:artist).permit(:name, :albums, :hometown, :img)
-        end
-        def find_artist
-            @artist = Artist.find(params[:id])
-        end
+  private
+    
+    def person_params
+      params.require(:artist).permit(:name, :albums, :hometown, :img)
+    end
 end
